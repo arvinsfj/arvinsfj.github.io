bcb451ffe9a441d98499d876827a3534033dffeb
arvin <arvin.cheng@fuwo.com>
2018-05-18 12:43:44 +0800

    update

--- public/ctt/Tools/jsc/my.js
+++ public/ctt/Tools/jsc/my.js
@@ -1,6 +1,6 @@
 
 
-content = '\
+content = "\
 #include <stdio.h>\
 int main() {\
     int score[6] = {76, 82, 90, 86, 79, 62};\
@@ -9,8 +9,8 @@ int main() {\
     float mean, sum;\
     int temp;\
     int i;\
-    printf("please input your student number:");\
-    scanf("%d" , &stu_number);\
+    printf(\"please input your student number:\");\
+    scanf(\"%d\" , &stu_number);\
     sum = 0;\
     temp = 0;\
     for(i = 0 ; i < 6 ; i++) {\
@@ -20,14 +20,14 @@ int main() {\
     mean = sum / temp ;\
     if(mean >= 60) {\
          mean = mean - 60 ;\
-         printf("the score of student number %d is %f higher than 60.\n", stu_number, mean);\
+         printf(\"the score of student number %d is %f higher than 60.\n\", stu_number, mean);\
     } else {\
          mean = 60 - mean ;\
-         printf( "the score of student number %d is %f lower than 60.\n", stu_number, mean );\
+         printf(\"the score of student number %d is %f lower than 60.\n\", stu_number, mean );\
     }\
     return 0;\
 }\
-';
+";
 
 var l = new Lexer();
 l.show();
